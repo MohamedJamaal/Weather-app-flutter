ad22cf3d19885654019f8b2055957f180f7a3fc6
mohamedjamaal <mohamed_gamal19@hotmail.com>
2024-08-30 20:39:52 +0300

    ADD app ui

---
+++ weather_app/lib/views/home_view.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/views/search_view.dart';
+import 'package:weather_app/widgets/no_weather_body.dart';
+// import 'package:weather_app/widgets/no_weather_body.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Weather App'),
+        backgroundColor: Colors.blueAccent,
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) {
+                    return const SearchView();
+                  },
+                ),
+              );
+            },
+            icon: const Icon(Icons.search),
+          ),
+        ],
+      ),
+      body: const NoWeatherBody(),
+    );
+  }
+}
---
+++ weather_app/lib/views/search_view.dart
@@ -0,0 +1,38 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+class SearchView extends StatelessWidget {
+  const SearchView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Search City'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Center(
+          child: TextField(
+            onSubmitted: (value) {
+              log(value);
+            },
+            decoration: const InputDecoration(
+              contentPadding: EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 32,
+              ),
+              labelText: 'Search',
+              suffixIcon: Icon(Icons.search),
+              hintText: 'Enter City Name',
+              border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.green),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
